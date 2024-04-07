c67249f72264975a62f82e08968d951698c63326
Rhaldon Cainto <145288481+AzertivL@users.noreply.github.com>
2024-04-07 23:57:16 +1000

    Jump mechanic and animation

--- Assets/Rhaldon/Scripts/PlayerMovement.cs
+++ Assets/Rhaldon/Scripts/PlayerMovement.cs
@@ -10,22 +10,54 @@ public class PlayerMovement : MonoBehaviour
     Vector2 movement;
     public float moveSpeed;
 
+    //Jump
+    Vector3 velocity;
+    public float jumpHeight;
+    public float gravity;
+    bool isGrounded;
+
+    //Animation
+    private Animator anim;
+
     // Start is called before the first frame update
     void Start()
     {
-      controller = GetComponent<CharacterController>();
+        anim = GetComponent<Animator>();
+        controller = GetComponent<CharacterController>();
     }
 
     // Update is called once per frame
     void Update()
     {
-       movement = new Vector2 (Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        isGrounded = Physics.CheckSphere(transform.position, .1f, 1);
+
+        if (isGrounded && velocity.y < 0) {
+            velocity.y = -1;
+        }
+
+        movement = new Vector2 (Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
         // Normalized is used to avoid player sprinting in a diagonal direction.
         Vector3 direction = new Vector3(movement.x, 0, movement.y).normalized;
 
         if (direction.magnitude >= 0.1f) {
             controller.Move(direction * moveSpeed * Time.deltaTime);
         }
+
+        // Jumping
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded) {
+            velocity.y = Mathf.Sqrt((jumpHeight * 10) * -2f * gravity);
+        }
+
+        if (direction != Vector3.zero)
+        {
+            anim.SetBool("isMoving", true);
+        }
+        else {
+            anim.SetBool("isMoving", false);
+        }
+
+        velocity.y += (gravity * 10) * Time.deltaTime;
+        controller.Move(velocity * Time.deltaTime);
     }
 
   
