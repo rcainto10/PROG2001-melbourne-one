a9f246204059bbf832150de09a1cdc13eaa21e7c
Rhaldon Cainto <145288481+AzertivL@users.noreply.github.com>
2024-04-08 11:44:46 +1000

    Sound/Player Rotation
    Implemented footstep sound effects and ambience.
    
    Fixed the player not rotating when walking back.

--- Assets/Rhaldon/Scripts/PlayerMovement.cs
+++ Assets/Rhaldon/Scripts/PlayerMovement.cs
@@ -1,34 +1,43 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using UnityEngine;
 
-// Youtube Guide Reference: https://www.youtube.com/watch?v=iHzAwGg--LM
+// Youtube Movement Guide Reference: https://www.youtube.com/watch?v=iHzAwGg--LM
+// Rotate Guide: https://www.youtube.com/watch?v=LnQudtIKfnw
 
 public class PlayerMovement : MonoBehaviour
 {
+    // Variables
     CharacterController controller;
     Vector2 movement;
-    public float moveSpeed;
+    [SerializeField] float moveSpeed;
 
     //Jump
     Vector3 velocity;
-    public float jumpHeight;
-    public float gravity;
-    bool isGrounded;
+    [SerializeField] float jumpHeight;
+    [SerializeField] float gravity;
+    [SerializeField] bool isGrounded;
 
     //Animation
     private Animator anim;
+
+    // Sound
+    // Footstep
+    public GameObject footsteps;
 
     // Start is called before the first frame update
     void Start()
     {
         anim = GetComponent<Animator>();
         controller = GetComponent<CharacterController>();
+        footsteps.SetActive(false);
     }
 
     // Update is called once per frame
     void Update()
     {
+        // Checking if player is grounded
         isGrounded = Physics.CheckSphere(transform.position, .1f, 1);
 
         if (isGrounded && velocity.y < 0) {
@@ -48,17 +57,41 @@ public class PlayerMovement : MonoBehaviour
             velocity.y = Mathf.Sqrt((jumpHeight * 10) * -2f * gravity);
         }
 
+        // Running Animation/Sound
         if (direction != Vector3.zero)
         {
+            // Rotate Player Character to the back if user clicks on S key.
+            if (Input.GetKeyDown(KeyCode.S))
+            {
+                transform.localEulerAngles = new Vector3(0f, 180f, 0f);
+            }
+            else if (Input.GetKeyDown(KeyCode.W)) {
+                transform.localEulerAngles = new Vector3(0f, 0f, 0f);
+            }
             anim.SetBool("isMoving", true);
+            // Play footstep sfx
+            footStep();
         }
         else {
+            // Disable Moving animaion
             anim.SetBool("isMoving", false);
+            // Disable Footsteps
+            stopFootStep();
         }
 
         velocity.y += (gravity * 10) * Time.deltaTime;
         controller.Move(velocity * Time.deltaTime);
     }
 
+    // Footstep functions
+    // Enable Footstep sfx function
+    void footStep() { 
+        footsteps.SetActive(true);
+    }
+
+    // Disable Footstep sfx
+    void stopFootStep() {
+        footsteps.SetActive(false);
+    }
   
 }
